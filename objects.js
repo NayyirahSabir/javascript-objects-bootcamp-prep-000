9bd30fcf90b30355f19389c05f601774b558c1f6
nsabir <awesomeartist15@gmail.com>
2019-08-28 23:01:39 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,7 @@
+var playlist = new Object({ artist: 'song name'})
+
+function updatePlaylist(playlist, artistName, songTitle){
+  updatePlaylist[artistName] = 'songTitle'
+  return updatePlaylist
+}
+
